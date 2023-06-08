3b2b6081ee97b8043d23cc03a0614312267661b6
pedro <palvarez122@hotmail.com>
2023-06-07 21:39:04 -0500

    Added calendar fragment to get range of dates inside statfragment

--- app/src/main/java/aandroid/paandroidportfolio/tiptracker/usecase/StatFragment.kt
+++ app/src/main/java/aandroid/paandroidportfolio/tiptracker/usecase/StatFragment.kt
@@ -7,11 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import aandroid.paandroidportfolio.tiptracker.R
 import aandroid.paandroidportfolio.tiptracker.ViewModel
+import android.app.DatePickerDialog
 import android.content.ContentValues.TAG
 import android.util.Log
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.TextView
 import androidx.fragment.app.activityViewModels
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class StatFragment : Fragment() {
 
@@ -20,19 +25,69 @@ class StatFragment : Fragment() {
     }
 
     private val sharedViewModel: ViewModel by activityViewModels()
-
+    private lateinit var dateTextView: TextView
+    var dateControl = 0
+    private var startDate = ""
+    private var endDate = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootview = inflater.inflate(R.layout.fragment_stats, container, false)
 
-        val dateTextView = rootview.findViewById<TextView>(R.id.tv_dateRange)
+        val calendar = Calendar.getInstance()
 
+        val datePicker = DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(calendar)
+        }
+
+        dateTextView = rootview.findViewById<TextView>(R.id.tv_dateRange)
+        val setDateRange = rootview.findViewById<Button>(R.id.setDateRange)
         dateTextView.text = sharedViewModel.date
+
+        setDateRange.setOnClickListener {
+            Log.d(TAG, "Setting Date Range")
+            dateControl = 0
+            context?.let { it1 ->
+                val datePickerDialog = DatePickerDialog(
+                    it1, datePicker, calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                datePickerDialog.setTitle("Choose end date")
+                datePickerDialog.show()
+            }
+            context?.let { it1 ->
+                val datePickerDialog = DatePickerDialog(
+                    it1, datePicker, calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                datePickerDialog.setTitle("Choose starting date")
+                datePickerDialog.show()
+            }
+
+        }
+
 
 
         return rootview
+    }
+
+
+    private fun updateLabel(calendar: Calendar) {
+        val myformat = "MM-dd-yyyy"
+        val sdf = SimpleDateFormat(myformat, Locale.US)
+        //Set the date text to the start
+    if(dateControl == 0){
+        dateTextView.setText(sdf.format(calendar.time))
+        startDate= sdf.format(calendar.time)
+        dateControl++
+        }
+        else
+        endDate= sdf.format(calendar.time)
+        Log.d(TAG, "Start-end:   " +startDate +"- " +endDate)
+
     }
 
 }
